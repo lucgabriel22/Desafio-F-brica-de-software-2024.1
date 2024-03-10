a89259d9c94ab640d066322eee04485ec2061325
luc <160556539+lucgabriel22@users.noreply.github.com>
2024-03-10 15:44:05 -0300

    🧱 Update urls.py
    retirado authenticador

--- desafio 1 - Consumir uma api e retornar/projeto1/urls.py
+++ desafio 1 - Consumir uma api e retornar/projeto1/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 from apiapp.api import viewsets as githubviewsets
     
@@ -11,9 +10,6 @@ route.register(f'git', githubviewsets.GithubViewSet, basename='Github')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apiapp.api.urls')),
-    path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
-    path('admin/', admin.site.urls)
 ]
 
 # rotas do autenticador
