896f17908269eaacabe53c4109d348c40b5b04df
luc <160556539+lucgabriel22@users.noreply.github.com>
2024-03-10 15:53:10 -0300

    🧱 Update settings.py
    Retirando mais uma vez o authenticador

--- desafio 1 - Consumir uma api e retornar/projeto1/settings.py
+++ desafio 1 - Consumir uma api e retornar/projeto1/settings.py
@@ -29,8 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apiapp',
-    'rest_framework',
-    'rest_framework_simplejwt',
+    'rest_framework'
+    
 ]
 
 
