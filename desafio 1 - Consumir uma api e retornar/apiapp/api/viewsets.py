15002c3e0e642bc4c22e5a0fad8e466225b59fc1
luc <160556539+lucgabriel22@users.noreply.github.com>
2024-03-10 15:41:51 -0300

    🧱 Update viewsets.py
    Mexido na Infraestrutura no viewsets.py que estava com "status_codigo" invés de "status_code"

--- desafio 1 - Consumir uma api e retornar/apiapp/api/viewsets.py
+++ desafio 1 - Consumir uma api e retornar/apiapp/api/viewsets.py
@@ -28,8 +28,8 @@ class GithubViewSet(ModelViewSet):
         site = f'https://api.github.com/users/{login}'
         requisicao = requests.get(site)
 
-        if requisicao.status_codigo != 200:
-            return Response({'aviso': 'Erro ao pesquisar o usuário no GitHub'}, status=requisicao.status_codigo)
+        if requisicao.status_code != 200:
+            return Response({'aviso': 'Erro ao pesquisar o usuário no GitHub'}, status=requisicao.status_code)
         
         json_data = requisicao.json()
 
@@ -68,8 +68,8 @@ class GithubViewSet(ModelViewSet):
         site = f'https://api.github.com/users/{login}'
         requisicao = requests.get(site)
 
-        if requisicao.status_codigo != 200:
-            return Response({'aviso': 'Usuário do GitHub não encontrado'}, status=requisicao.status_codigo)
+        if requisicao.status_code != 200:
+            return Response({'aviso': 'Usuário do GitHub não encontrado'}, status=requisicao.status_code)
 
         json_data = requisicao.json()
         serializer = GithubSerializer(data=json_data)
@@ -87,8 +87,8 @@ class GithubViewSet(ModelViewSet):
         site = f'https://api.github.com/users/{login}'
         requisicao = requests.put(site)
 
-        if requisicao.status_codigo != 200:
-            return Response({'aviso': 'Erro ao atualizar o usuário no GitHub'}, status=requisicao.status_codigo)
+        if requisicao.status_code != 200:
+            return Response({'aviso': 'Erro ao atualizar o usuário no GitHub'}, status=requisicao.status_code)
 
         json_data = requisicao.json()
         serializer = GithubSerializer(instance=self.get_object(), data=json_data)
@@ -107,8 +107,8 @@ class GithubViewSet(ModelViewSet):
         site = f'https://api.github.com/users/{login}'
         requisicao = requests.patch(site)
 
-        if requisicao.status_codigo != 200:
-            return Response({'aviso': 'Erro ao atualizar parcialmente o usuário no GitHub'}, status=requisicao.status_codigo)
+        if requisicao.status_code != 200:
+            return Response({'aviso': 'Erro ao atualizar parcialmente o usuário no GitHub'}, status=requisicao.status_code)
 
         json_data = requisicao.json()
         serializer = GithubSerializer(instance=self.get_object(), data=json_data)
@@ -129,8 +129,8 @@ class GithubViewSet(ModelViewSet):
         site = f'https://api.github.com/users/{user.login}'
         requisicao = requests.delete(site)
 
-        if requisicao.status_codigo == 204:
+        if requisicao.status_code == 204:
             user.delete()
             return Response({'aviso': 'Usuário excluído com sucesso'}, status=204)
         else:
-            return Response({'aviso': 'Erro ao excluir o usuário do GitHub'}, status=requisicao.status_codigo)
+            return Response({'aviso': 'Erro ao excluir o usuário do GitHub'}, status=requisicao.status_code)
